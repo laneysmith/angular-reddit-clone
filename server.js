649cc3a1e6939fa7e5a383aef85e0433dcb419ff
Laney Smith <lsmith.unc@gmail.com>
2016-07-22 08:55:16 -0600

    Set up express

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(request, response, next) {
+ response.sendFile('index.html');
+});
+
+app.listen(process.env.PORT || 1337);
